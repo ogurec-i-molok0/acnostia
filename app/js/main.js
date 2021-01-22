6793bbe595a2577c8086f115c81d0c1cd36b35c3
ogurec-i-molok0 <a.manczurow@yandex.ru>
2021-01-22 18:55:33 +0300

    header and first

---
+++ app/js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('.first__slider-inner').slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: '<button class="slick-prev slick-arrow"><img src="../images/arrow-prew.png" alt=""> </button>',
+        nextArrow: '<button class="slick-prev slick-arrow"><img src="../images/arrow-next.png" alt=""> </button>'
+    });
+});
